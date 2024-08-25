2fc556b20895bd142ab82bf0e3fdf4abf9ab25a5
sheilarlauro <sheilar.lauro@gmail.com>
2024-08-25 19:02:00 -0300

    Codigo da atividade

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.print("Por favor, digite o número da Conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = scanner.next();
+
+        System.out.print("Digite o nome do Cliente: ");
+        String nomeCliente = scanner.next();
+
+        System.out.print("Digite seu saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia + ", conta " + numero + " e seu saldo "+ saldo + " já está disponível para saque.");
+
     }
-}
+    }
